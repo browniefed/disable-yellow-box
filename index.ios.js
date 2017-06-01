6a5a92ff37cef68b644c0cb0f1e6056404ac3acd
Jason Brown <browniefed@gmail.com>
2017-05-31 20:41:09 -0700

    Add code

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,15 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { AppRegistry, StyleSheet, Text, View, TextInput } from "react-native";
 
-const PropTypeTest = ({ value }) => <View><Text>{value}</Text></View>;
+console.ignoredYellowBox = ['Warning: Each', "Warning: Failed prop type"];
+
+const PropTypeTest = ({ value }) => <View><Text>{value}</Text></View>
 
 PropTypeTest.propTypes = {
   value: PropTypes.string.isRequired,
-};
+}
 export default class disable_yellow_box extends Component {
   render() {
     const arrayOfItems = ["Text", "Text2", "Text3"];
@@ -27,8 +29,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
-// console.disableYellowBox = true;
-console.ignoredYellowBox = ['Warning: Each'];
 
 AppRegistry.registerComponent("disable_yellow_box", () => disable_yellow_box);
