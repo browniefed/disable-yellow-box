330a6fc933e908fad77d402563b034db5e5058e3
Jason Brown <browniefed@gmail.com>
2017-05-31 19:58:34 -0700

    Add in examples

--- index.ios.js
+++ index.ios.js
@@ -1,22 +1,19 @@
-import React, { Component, PropTypes } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput
-} from 'react-native';
+import React, { Component, PropTypes } from "react";
+import PropTypes from "prop-types";
 
+import { AppRegistry, StyleSheet, Text, View, TextInput } from "react-native";
 
+const PropTypeTest = ({ value }) => <View><Text>{value}</Text></View>;
+
+PropTypeTest.propTypes = {
+  value: PropTypes.string.isRequired,
+};
 export default class disable_yellow_box extends Component {
   render() {
-
-    const arrayOfItems = ["Text", "Text2", "Text3"]
+    const arrayOfItems = ["Text", "Text2", "Text3"];
     return (
       <View style={styles.container}>
-        {
-          arrayOfItems.map(text => <Text>{text}</Text>)
-        }
+        {arrayOfItems.map(text => <Text>{text}</Text>)}
         <PropTypeTest value={5} />
       </View>
     );
@@ -26,10 +23,12 @@ export default class disable_yellow_box extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
+// console.disableYellowBox = true;
+console.ignoredYellowBox = ['Warning: Each'];
 
-AppRegistry.registerComponent('disable_yellow_box', () => disable_yellow_box);
+AppRegistry.registerComponent("disable_yellow_box", () => disable_yellow_box);
